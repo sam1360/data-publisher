a1357b82a16f7ea7196e31c27f01855fbbe0928a
Sam Mosher <sam@nate-VirtualBox.(none)>
2015-10-20 17:04:09 -0400

    A couple more tests Signed-off-by: Sam Mosher <sam@nate-VirtualBox.(none)>

---
+++ src/ODR/AdminBundle/Tests/Controller/RecordControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sam
+ * Date: 10/8/15
+ * Time: 8:38 AM
+ */
+namespace ODR\AdminBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class RecordControllerTest extends WebTestCase
+{
+    public static $client = "";
+    public $token = "";
+
+    /**
+     * Test the functionality for the radio selection datfields.
+     */
+    public function testRadioSelectionAction()
+    {
+
+    }
+
+    /**
+     * Test functionality for adding a data record.
+     */
+    public function testAddAction()
+    {
+
+    }
+    /**
+     * Tests the programs ability to add child records.
+     */
+    public function testAddChildRecordAction()
+    {
+
+    }
+
+    /**
+     * testing the ability to delete a datarecord.
+     */
+    public function testDeleteAction()
+    {
+
+    }
+
+
+
+}
